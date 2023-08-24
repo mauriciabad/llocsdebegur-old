3ac7bca563c1754b1a8185699accca6744d4a21c
mauriciabad <hello@mauriciabad.com>
2023-08-24 03:32:30 +0200

    move "up" word in command

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -2,4 +2,4 @@ echo "Building and deploying..."
 echo "Expect it to take arround 3 minutes..."
 echo ""
 
-docker compose up -f docker-compose.yaml -f docker-compose.prod.yaml -d --build
+docker compose -f docker-compose.yaml -f docker-compose.prod.yaml up -d --build
