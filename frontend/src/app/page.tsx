d0613065a94d94ec95a9bb112e9e761c9c959fc7
mauriciabad <hello@mauriciabad.com>
2023-08-26 01:58:14 +0200

    Add prettier

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,30 +1,34 @@
-import { graphqlClient } from "@/lib/graphql"
-import { gql } from "graphql-request"
+import { graphqlClient } from '@/lib/graphql'
+import { gql } from 'graphql-request'
 
 const queryGetAllBeaches = gql`
-query{
-  detailsBeaches {
-    data {
-      attributes {
-        basicDetails {
-          id
-          name
-          shortDescription
+  query {
+    detailsBeaches {
+      data {
+        attributes {
+          basicDetails {
+            id
+            name
+            shortDescription
+          }
         }
       }
     }
   }
-}
 `
 
 export default async function Home() {
-  const beaches = await graphqlClient.request(queryGetAllBeaches) as any
+  const beaches = (await graphqlClient.request(queryGetAllBeaches)) as any
 
   return (
     <main>
       <h1>Platjes</h1>
       <ul>
-        {beaches.detailsBeaches.data.map(beach => <li key={beach.attributes.basicDetails.id}>{beach.attributes.basicDetails.name}</li>)}
+        {beaches.detailsBeaches.data.map((beach) => (
+          <li key={beach.attributes.basicDetails.id}>
+            {beach.attributes.basicDetails.name}
+          </li>
+        ))}
       </ul>
     </main>
   )
--- frontend/src/lib/graphql.ts
+++ frontend/src/lib/graphql.ts
@@ -1,7 +1,8 @@
-import { GraphQLClient } from "graphql-request";
+import { GraphQLClient } from 'graphql-request'
 
-const GRAPHQL_API_URL = process.env.NODE_ENV === 'production'
-  ? 'https://cms.llocsdebegur.s.mauriciabad.com/graphql'
-  : 'http://localhost:1337/graphql'
+const GRAPHQL_API_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://cms.llocsdebegur.s.mauriciabad.com/graphql'
+    : 'http://localhost:1337/graphql'
 
 export const graphqlClient = new GraphQLClient(GRAPHQL_API_URL)
