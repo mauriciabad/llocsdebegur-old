b2ddf757c2ddd714631b1f3992e200e4c0527c40
mauriciabad <hello@mauriciabad.com>
2023-08-25 22:40:55 +0200

    Improve env lib

--- frontend/src/lib/env.ts
+++ frontend/src/lib/env.ts
@@ -1,6 +1,6 @@
 const ENV_DEFAULTS = {
-  GRAPHQL_API_KEY: 'any-key-works',
-  GRAPHQL_API_URL: 'http://localhost:1337/graphql'
+  GRAPHQL_API_KEY: null, // Login in strapi admin, go to settings and generate a new key
+  GRAPHQL_API_URL: 'http://localhost:1337'
 } as const
 
 type EnvValidKey = keyof typeof ENV_DEFAULTS
@@ -11,6 +11,10 @@ export function env(name: EnvValidKey): string {
     if (value === undefined) throw new Error(`Missing environment variable ${name}. You are running in production and didn't create the propper .env file. Create the propper .env file, check the README.md about how to do so.`)
     return value
   } else {
-    return ENV_DEFAULTS[name]
+    const envValue = process.env[name]
+    const defaultValue = ENV_DEFAULTS[name]
+    const value = envValue ?? defaultValue
+    if (!value) throw new Error(`Missing environment variable ${name}. You are running in development and didn't create the propper .env file.`)
+    return value
   }
 }
