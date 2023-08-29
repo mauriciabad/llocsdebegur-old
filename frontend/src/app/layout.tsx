5ae62862cc33790fc0cb8bd891c1f19750c0b730
mauriciabad <hello@mauriciabad.com>
2023-08-29 21:01:47 +0200

    Error screen

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import classNames from 'classnames'
-import { useLocale } from 'next-intl'
+import { NextIntlClientProvider, useLocale } from 'next-intl'
 import { ApolloWrapper } from './ApolloWrapper'
 import Footer from '@/components/Footer'
 const inter = Inter({ subsets: ['latin'] })
@@ -18,12 +18,14 @@ export default async function RootLayout({
   children: React.ReactNode
 }) {
   const locale = useLocale()
+  const messages = (await import(`../../messages/${locale}.json`)).default
 
   return (
     <html lang={locale}>
       <body className={classNames([inter.className, 'min-h-screen'])}>
-        <ApolloWrapper>{children}</ApolloWrapper>
-
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          <ApolloWrapper>{children}</ApolloWrapper>
+        </NextIntlClientProvider>
         <Footer />
       </body>
     </html>
