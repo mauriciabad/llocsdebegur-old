fcb240b3378a2e0e89f0b17f9046254f00235ba7
mauriciabad <hello@mauriciabad.com>
2023-08-31 00:57:54 +0200

    Fix leaflet console error

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -1,71 +1,14 @@
-'use client'
+import dynamic from 'next/dynamic'
+import { IconLoader } from '@tabler/icons-react'
 
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
-import type Leaflet from 'leaflet'
-import 'leaflet/dist/leaflet.css'
-import classNames from 'classnames'
-import { Icon } from 'leaflet'
-import IconMapPin from '/public/icon-map-pin.svg'
-
-type Location = {
-  latitude: number
-  longitude: number
-}
-
-function toLatLang(location: Location): Leaflet.LatLngLiteral
-function toLatLang(
-  location: Location | undefined
-): Leaflet.LatLngLiteral | undefined
-function toLatLang(
-  location: Location | undefined
-): Leaflet.LatLngLiteral | undefined {
-  if (!location) return undefined
-  return { lat: location.latitude, lng: location.longitude }
-}
-
-export default function Map({
-  location,
-  className,
-  markers,
-}: {
-  location: Location
-  className?: string
-  markers?: {
-    location: Location
-    text?: string
-  }[]
-}) {
-  return (
-    <div>
-      <MapContainer
-        center={toLatLang(location)}
-        zoom={13}
-        scrollWheelZoom={false}
-        className={classNames(className, 'z-0 h-64 w-full')}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {markers?.map(({ text, location: { latitude, longitude } }) => (
-          <Marker
-            key={`${latitude}-${longitude}`}
-            position={{
-              lat: latitude,
-              lng: longitude,
-            }}
-            icon={
-              new Icon({
-                iconUrl: IconMapPin.src,
-                iconAnchor: [94 * 0.4 * 0.5, 128 * 0.4 * 1],
-                iconSize: [94 * 0.4, 128 * 0.4],
-              })
-            }
-          >
-            {text && <Popup>{text}</Popup>}
-          </Marker>
-        ))}
-      </MapContainer>
+const Map = dynamic(() => import('./MapRaw'), {
+  loading: () => (
+    <div className="flex h-64 w-full items-center justify-center bg-[#f2efe9]">
+      <IconLoader className="h-4 w-4 animate-spin" />
+      <span className="animate-pulse">Loading map...</span>
     </div>
-  )
-}
+  ),
+  ssr: false,
+})
+
+export default Map
