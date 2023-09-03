819121b35885e72b19a0c6dccb9ac0aaf45bd94c
mauriciabad <hello@mauriciabad.com>
2023-09-04 01:00:57 +0200

    fix to generate all blurhashes on the existing images

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,5 +14,28 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  // bootstrap(/*{ strapi }*/) {},
+  // TODO: Remove this is a fix to generate all blurhashes on the existing images. It should be done my now
+  bootstrap() {
+    const updateAllMediaFiles = async () => {
+      const uploads = await strapi.query("plugin::upload.file").findMany();
+      for (const item of uploads) {
+        if (item.mime && item.mime.startsWith("image/") && !item.blurhash) {
+          const blurhash = await strapi
+            .plugin("strapi-blurhash")
+            .service("blurhash")
+            .generateBlurhash(item.url);
+          await strapi.db.query("plugin::upload.file").update({
+            where: { id: item.id },
+            data: {
+              blurhash,
+            },
+          });
+        }
+      }
+      strapi.log.debug("Finished generating blurhash for the library");
+    };
+
+    updateAllMediaFiles();
+  },
 };
