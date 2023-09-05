3f466019b9e43ed7952f4aa9e1468b135eaaaeab
mauriciabad <hello@mauriciabad.com>
2023-09-06 00:31:13 +0200

    add link

--- frontend/src/app/[locale]/(map)/map/page.tsx
+++ frontend/src/app/[locale]/(map)/map/page.tsx
@@ -12,6 +12,7 @@ import {
 } from '@/lib/gql'
 import { DeepPick } from '@/lib/types'
 import { useLocale, useTranslations } from 'next-intl'
+import Link from 'next-intl/link'
 import { notFound } from 'next/navigation'
 
 const getAllPlacesQuery = graphql(`
@@ -88,36 +89,41 @@ function SubPage({
     <div className="h-[calc(100dvh)]!important grid h-screen grid-cols-[475px,auto]">
       <aside className="z-10 h-full overflow-auto shadow-lg">
         <Header />
-        <div className="p-4">
+        <div className="p-2">
           <h1 className="mb-4 text-center text-2xl font-bold">{t('map')}</h1>
-          <ul className="space-y-4">
+          <ul className="">
             {places.map((place) => (
               <li key={place.slug} className="">
-                <article className="grid grid-cols-[1fr,auto]">
-                  <div className="pr-4">
-                    <h1 className="font-title text-lg font-semibold text-stone-800">
-                      {place.name}
-                    </h1>
-                    <span className="rounded border border-stone-200 bg-stone-100 px-2 py-1 text-xs font-medium capitalize text-stone-700">
-                      <PlaceIcon
-                        type={place.type?.slug}
-                        className="mr-1.5 inline-block h-4 w-4 align-text-top text-stone-500 [&>path]:stroke-1"
-                      />
-                      {place.type?.name}
-                    </span>
+                <Link
+                  href={`/${place.type?.slug ?? 'null'}/${place.slug}`}
+                  className="group"
+                >
+                  <article className="grid grid-cols-[1fr,auto] rounded-xl border border-transparent p-2 group-hover:bg-gray-100">
+                    <div className="pr-4">
+                      <h1 className="font-title text-lg font-semibold leading-none text-stone-800">
+                        {place.name}
+                      </h1>
+                      <span className="mt-1 inline-block rounded border border-stone-200 bg-stone-100 px-2 py-1 text-xs font-medium capitalize text-stone-700">
+                        <PlaceIcon
+                          type={place.type?.slug}
+                          className="mr-1.5 inline-block h-4 w-4 align-text-top text-stone-500 [&>path]:stroke-1"
+                        />
+                        {place.type?.name}
+                      </span>
 
-                    <p className="line-clamp-2">{place.description}</p>
-                  </div>
-                  {place.cover ? (
-                    <StrapiImage
-                      height={96}
-                      image={place.cover}
-                      className="aspect-square w-24 rounded-lg object-cover shadow-md"
-                    />
-                  ) : (
-                    <div className="aspect-square w-24 rounded-lg object-cover shadow-md" />
-                  )}
-                </article>
+                      <p className="line-clamp-2">{place.description}</p>
+                    </div>
+                    {place.cover ? (
+                      <StrapiImage
+                        height={96}
+                        image={place.cover}
+                        className="aspect-square w-24 rounded-lg object-cover shadow-md"
+                      />
+                    ) : (
+                      <div className="aspect-square w-24 rounded-lg object-cover shadow-md" />
+                    )}
+                  </article>
+                </Link>
               </li>
             ))}
           </ul>
