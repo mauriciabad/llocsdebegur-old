68915f9a2661185c2aa31590dcc82ee9a8e1c79d
mauriciabad <hello@mauriciabad.com>
2023-09-07 15:09:16 +0200

    Add header to 500

--- frontend/src/app/error.tsx
+++ frontend/src/app/error.tsx
@@ -4,6 +4,7 @@ import Link from 'next-intl/link'
 
 import { useTranslations } from 'next-intl'
 import { useEffect } from 'react'
+import Header from '@/components/Header'
 
 export default function Error({
   error,
@@ -19,56 +20,59 @@ export default function Error({
   }, [error])
 
   return (
-    <section className="bg-white">
-      <div className="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
-        <div className="mx-auto max-w-screen-sm text-center">
-          <h1 className="text-primary-600 mb-4 font-title text-7xl font-extrabold tracking-tight lg:text-9xl">
-            500
-          </h1>
-          <p className="mb-8 font-title text-3xl font-bold tracking-tight text-stone-900 md:text-4xl">
-            {t('internal-server-error')}
-          </p>
+    <>
+      <Header />
+      <section className="bg-white">
+        <div className="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
+          <div className="mx-auto max-w-screen-sm text-center">
+            <h1 className="text-primary-600 mb-4 font-title text-7xl font-extrabold tracking-tight lg:text-9xl">
+              500
+            </h1>
+            <p className="mb-8 font-title text-3xl font-bold tracking-tight text-stone-900 md:text-4xl">
+              {t('internal-server-error')}
+            </p>
 
-          <p className="mb-2 text-lg font-light text-stone-500">
-            {t('message')}
-          </p>
-          <p className="mx-auto mb-8 inline-block rounded-md bg-stone-200 p-2 font-mono font-bold leading-none">
-            {error.digest}
-          </p>
+            <p className="mb-2 text-lg font-light text-stone-500">
+              {t('message')}
+            </p>
+            <p className="mx-auto mb-8 inline-block rounded-md bg-stone-200 p-2 font-mono font-bold leading-none">
+              {error.digest}
+            </p>
 
-          <p className="mb-2 text-lg font-light text-stone-500">
-            {t('error-message')}
-          </p>
-          <pre className="mb-8 whitespace-normal break-normal rounded-md bg-stone-800 px-6 py-4 text-left font-mono text-sm text-white">
-            {error.message}
-          </pre>
+            <p className="mb-2 text-lg font-light text-stone-500">
+              {t('error-message')}
+            </p>
+            <pre className="mb-8 whitespace-normal break-normal rounded-md bg-stone-800 px-6 py-4 text-left font-mono text-sm text-white">
+              {error.message}
+            </pre>
 
-          <p className="mb-2 text-lg font-light text-stone-500">
-            {t('actions')}
-          </p>
-          <div>
-            <Link
-              href="/"
-              className="mb-2 mr-2 rounded-full bg-brand-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-brand-800 focus:outline-none focus:ring-4 focus:ring-brand-300"
-            >
-              {t('go-home')}
-            </Link>
-            <button
-              className="mb-2 mr-2 rounded-full bg-brand-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-brand-800 focus:outline-none focus:ring-4 focus:ring-brand-300"
-              onClick={() => reset()}
-            >
-              {t('try-again')}
-            </button>
-            <a
-              href="mailto:hello@mauriciabad.com"
-              target="_blank"
-              className="mb-2 mr-2 rounded-full bg-brand-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-brand-800 focus:outline-none focus:ring-4 focus:ring-brand-300"
-            >
-              {t('contact-support')}
-            </a>
+            <p className="mb-2 text-lg font-light text-stone-500">
+              {t('actions')}
+            </p>
+            <div>
+              <Link
+                href="/"
+                className="mb-2 mr-2 rounded-full bg-brand-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-brand-800 focus:outline-none focus:ring-4 focus:ring-brand-300"
+              >
+                {t('go-home')}
+              </Link>
+              <button
+                className="mb-2 mr-2 rounded-full bg-brand-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-brand-800 focus:outline-none focus:ring-4 focus:ring-brand-300"
+                onClick={() => reset()}
+              >
+                {t('try-again')}
+              </button>
+              <a
+                href="mailto:hello@mauriciabad.com"
+                target="_blank"
+                className="mb-2 mr-2 rounded-full bg-brand-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-brand-800 focus:outline-none focus:ring-4 focus:ring-brand-300"
+              >
+                {t('contact-support')}
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   )
 }
