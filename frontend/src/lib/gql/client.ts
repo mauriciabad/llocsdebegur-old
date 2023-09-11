f85b970a3fe84eaf7e54fb1bdc26a79d22375e3e
mauriciabad <hello@mauriciabad.com>
2023-09-11 19:31:30 +0200

    add Bearer token

--- frontend/src/lib/gql/client.ts
+++ frontend/src/lib/gql/client.ts
@@ -1,4 +1,5 @@
 import { BACKEND_URL, IS_PRODUCTION_ENV } from '@/constants'
+import { getServerSessionCustom } from '@/services/authentication'
 import { ApolloLink, HttpLink } from '@apollo/client'
 import { loadDevMessages, loadErrorMessages } from '@apollo/client/dev'
 import {
@@ -6,6 +7,8 @@ import {
   NextSSRInMemoryCache,
   SSRMultipartLink,
 } from '@apollo/experimental-nextjs-app-support/ssr'
+import { setContext } from '@apollo/client/link/context'
+import { getSession } from 'next-auth/react'
 
 if (!IS_PRODUCTION_ENV) {
   loadDevMessages()
@@ -19,16 +22,27 @@ export function gqlClient() {
     uri: GRAPHQL_API_URL,
   })
 
+  const authLink = setContext(async () => {
+    const session = await getSession()
+    if (!session?.jwt) return {}
+    return {
+      headers: {
+        Authorization: `Bearer ${session.jwt}`,
+      },
+    }
+  })
+
   return new NextSSRApolloClient({
     cache: new NextSSRInMemoryCache(),
     link:
       typeof window === 'undefined'
         ? ApolloLink.from([
+            authLink,
             new SSRMultipartLink({
               stripDefer: true,
             }),
             httpLink,
           ])
-        : httpLink,
+        : ApolloLink.from([authLink, httpLink]),
   })
 }
